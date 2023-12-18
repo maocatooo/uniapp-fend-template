8b98789c1f818048a76c07c927ce4758641f4e50
maocat <zk246422@163.com>
2023-12-18 22:51:38 +0800

    添加一个小程序登录示例，Tailwindcss微信问题修改

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,3 @@
-<template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-      <uni-badge text="13322222211" type="error"></uni-badge>
-      <uni-badge :text="uniBadge2Text" type="success" @click="bindClick"></uni-badge>
-      <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
-  </view>
-    <view class="bg-blue-500 text-white p-4">
-        This is a UniApp component with Tailwind CSS!
-    </view>
-</template>
 
 <script setup  lang="ts">
 
@@ -24,7 +10,46 @@ const bindClick = () => {
   uniBadge2Text.value ++
 }
 
+const login = () => {
+  uni.login({
+    provider: "weixin", //使用微信登录
+    success: async function (loginRes) {
+      console.log(loginRes);
+      uni.request({
+      url: "http://127.0.0.1:5000/api/auth/login_wechat_mini_program",
+        method: "POST",
+        data: {
+        "code":loginRes.code,
+      }
+      }).then((res)=>{
+        console.log(res);
+        
+      })
+    },
+  });
+};
+
 </script>
+
+
+<template>
+  <view class="content">
+    <image class="logo" src="/static/logo.png" />
+    <view class="text-area w-1/2">
+      <text class="title">{{ title }}</text>
+    </view>
+      <uni-badge text="13322222211" type="error"></uni-badge>
+      <uni-badge :text="uniBadge2Text" type="success" @click="bindClick"></uni-badge>
+      <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+  </view>
+    <view class="bg-blue-500 text-white p-4" @click="login">
+        login
+    </view>
+
+    <view class="bg-blue-500 text-white p-4">
+        This is a UniApp component with Tailwind CSS!
+    </view>
+</template>
 
 <style>
 .content {
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,19 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import { resolve } from 'node:path'
 import AutoImport from 'unplugin-auto-import/vite'
+import {UnifiedViteWeappTailwindcssPlugin as uvwt} from 'weapp-tailwindcss/vite'
 import postcss from './postcss.config'
+
+console.log(process.env.UNI_PLATFORM === 'mp-weixin')
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        // 指定输出路径
+        dir: 'dist/dev/mp-weixin', // 你想要的输出路径
+      },
+    },
+  },
   plugins: [
     AutoImport({
       imports: ['vue', 'uni-app'],
@@ -12,7 +23,8 @@ export default defineConfig({
         enabled: true
       }
     }),
-    uni()
+    uni(),
+    uvwt({disabled:process.env.UNI_PLATFORM !== 'mp-weixin'})
   ],
   css: {
     postcss
